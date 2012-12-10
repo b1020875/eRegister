be3453580c5cd513a618a999a991ca7ea3bac61d
Frankie Preston <b1020875@my.shu.ac.uk>
2012-12-10 17:31:04 +0000

    Created initial domain model

---
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
@@ -0,0 +1,10 @@
+package uk.ac.shu.webarch.eregister
+
+class Instructor {
+
+String name
+String staffNumber
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Student.groovy
@@ -0,0 +1,10 @@
+package uk.ac.shu.webarch.eregister
+
+class Student {
+
+	String name
+	String studentNumber
+
+    static constraints = {
+    }
+}
